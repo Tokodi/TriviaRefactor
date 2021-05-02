6da94bd4565bfbec587f83dce158e9d6470a3405
Tokodi Andras <tokodiaa@gmail.com>
2021-05-02 18:09:10 +0200

    Remove fix seeds. Now the simulation is random

--- controller/controller.h
+++ controller/controller.h
@@ -24,12 +24,14 @@ private:
     bool isCorrectAnswer();
 
 private:
-    Model::Game    _game{4};
+    // NOTE: Hash test settings
+    //Model::Game    _game{4};
+    //std::mt19937 _generator{1};
+
+    Model::Game    _game{};
     View::TextView _view{_game};
 
-    // TODO: Fixed seed while developement
-    std::mt19937 _generator{1};
-    // std::mt19937 _generator{static_cast<std::uint64_t>(std::chrono::system_clock::now().time_since_epoch().count())};
+    std::mt19937 _generator{static_cast<std::uint64_t>(std::chrono::system_clock::now().time_since_epoch().count())};
     std::bernoulli_distribution _distribution{PROBABILITY_OF_CORRECT_ANSWER};
 };
 
